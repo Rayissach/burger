5e5fee39a26841949bd27609315c5fd5a480717d
Raymond Robinson <rayissach@raymonds-mbp.attlocal.net>
2018-03-09 00:25:35 -0800

    readding files and routes

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -2,13 +2,13 @@ var express = require("express");
 
 var router = express.Router();
 
-var burgers = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
 router.get("/", function(req, res){
 	//get all the data from burgers
 burger.all(function(data){
 	var brgObject = {
-		burgers:data
+		burgers: data
 	};
 	console.log(brgObject);
 	res.render("index", brgObject)
@@ -26,7 +26,7 @@ router.post("/api/burgers", function(req, res) {
 });
 
 router.put("/api/burgers/:id", function(req, res) {
-	var condition = "id= " + req.params.id;
+	var condition = "id = " + req.params.id;
 
 	console.log("condition" + condition);
 
@@ -45,7 +45,7 @@ router.put("/api/burgers/:id", function(req, res) {
 router.delete("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  burgers.delete(condition, function(result) {
+  burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-	id INT(45) AUTO_INCREMENT NOT NULL,
-	burger_name VARCHAR(100) NOT NULL,
+	id INT AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR (100) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY(id)
 	);
